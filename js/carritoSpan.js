b951fa4d150a071d9f55d841b1870973cd8f99cc
Omar <omarcory2@gmail.com>
2024-11-20 01:58:22 -0600

    Arreglado error en icono de carrito

--- js/carritoSpan.js
+++ js/carritoSpan.js
@@ -1,5 +1,4 @@
 const spanCarrito = document.getElementById('carritoIcon');
-
 //var badge = document.createElement('span');
 // import * as dateBase from "./base_Datos.js";
 
@@ -13,7 +12,7 @@ export function contadorCarrito(){
     carrito = JSON.parse(localStorage.getItem('carrito'));
     spanCarrito.innerHTML = "";
     let imgCarrito = document.createElement('IMG');
-    imgCarrito.setAttribute('src', '../assets/icons/iconoCarrito.png');
+    imgCarrito.setAttribute('src', './assets/icons/iconoCarrito.png');
     imgCarrito.setAttribute('id', 'iconoCarrito');
     let badge = document.createElement('span');
     badge.classList.add('position-absolute', 'translate-middle', 'badge', 'rounded-pill', 'bg-danger');
