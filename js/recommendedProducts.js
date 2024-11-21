a7bd831a079b6854057c11153a605fd1e3ced500
Omar <omarcory2@gmail.com>
2024-11-21 17:33:32 -0600

    Se agrega modelo de datos para generar recomendaciones

--- js/recommendedProducts.js
+++ js/recommendedProducts.js
@@ -1,15 +1,22 @@
-import { loMasPedido, añadirCarrito } from './base_Datos.js';
+import { loMasPedido } from './base_Datos.js';
 import { agregarProducto } from './CarritoCompras2.js';
 
 const container = document.querySelector('.image-container');
 
 const renderProducts = async () => {
+    var misProd = []
     container.innerHTML = `
         <div class="loader"></div>
     `;
-    await getProducts();
+    misProd = await getProducts();
     container.innerHTML = '';
-    loMasPedido.forEach(product => {
+    if (!Array.isArray(misProd) || misProd.length === 0) {
+        misProd = []
+        for (let i = 0; i < 3; i++) {
+            misProd.push(loMasPedido[i])
+        }
+    }
+    misProd.forEach(product => {
         const productDiv = document.createElement('div');
         productDiv.classList.add('product-card');
         productDiv.innerHTML = `
@@ -26,8 +33,39 @@ const renderProducts = async () => {
     });
 };
 
-const getProducts = async () => {
-    return new Promise(resolve => setTimeout(resolve, 3000));
+const getIds = () => {
+    var arrayIds = [];
+    if (localStorage.key(0) === null) {
+        localStorage.setItem('carrito', JSON.stringify([]));
+    }
+    var carrito = JSON.parse(localStorage.getItem('carrito'));
+    carrito.forEach(prod => {
+        arrayIds.push(prod.id);
+    })
+    return arrayIds;
 }
 
-await renderProducts(loMasPedido);
+const getProducts = async () => {
+    var ids = getIds();
+    if(ids.length == 0) {
+        ids = [16, 8];
+    }
+    try {
+        const response = await fetch('https://modelo-datos-modular-95e96ca4ce9f.herokuapp.com/recomendaciones', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                ids_articulos: ids
+            })
+        });
+        const recomendaciones = await response.json();
+        return recomendaciones;
+    } catch (error) {
+        console.log(`Error al consumir datos: ${error}`)
+        return []
+    }
+};
+
+await renderProducts();
